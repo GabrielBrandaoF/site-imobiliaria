f0695ea1e94956f3217c34e94a6d4c7cdefa1577
Gabriel Brandão <dev.gbrandao@gmail.com>
2026-08-01 15:08:38 -0300

    versao com js2

--- script.js
+++ script.js
@@ -27,4 +27,74 @@ if (avatar) {
 
     });
 
+/* ===========================
+   Partículas
+=========================== */
+
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
+
+let w;
+let h;
+let particles = [];
+
+function resize(){
+
+    w = canvas.width = window.innerWidth;
+    h = canvas.height = window.innerHeight;
+
+}
+
+resize();
+
+window.addEventListener("resize", resize);
+
+for(let i=0;i<22;i++){
+
+    particles.push({
+
+        x:Math.random()*w,
+        y:Math.random()*h,
+
+        r:Math.random()*2+1,
+
+        speed:Math.random()*0.35+0.1,
+
+        alpha:Math.random()*0.5
+
+    });
+
+}
+
+function animate(){
+
+    ctx.clearRect(0,0,w,h);
+
+    particles.forEach(p=>{
+
+        p.y-=p.speed;
+
+        if(p.y<0){
+
+            p.y=h;
+            p.x=Math.random()*w;
+
+        }
+
+        ctx.beginPath();
+
+        ctx.arc(p.x,p.y,p.r,0,Math.PI*2);
+
+        ctx.fillStyle=`rgba(198,161,91,${p.alpha})`;
+
+        ctx.fill();
+
+    });
+
+    requestAnimationFrame(animate);
+
+}
+
+animate();
+
 }
